4de073f8e2f75d24a483221273d69ef4e497f257
Timo Wilhelm <mail@timowilhelm.com>
2023-08-29 09:23:40 +0200

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Context
+
+This is a minimal reproduction of a bug in the `miniflare` library.
+Putting R2 blobs get stuck indefinitely in using the `getBindigns()` ProxyStub.
+
+# How to run
+> `npm install && npm run start`
---
+++ index.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+import { Miniflare } from 'miniflare';
+
+const mf = new Miniflare({
+  script: `
+  addEventListener("fetch", (event) => {
+    event.respondWith(new Response("Hello Miniflare!"));
+  });
+  `,
+  r2Buckets: {
+    R2_DEFAULT: 'default',
+  },
+  r2Persist: './.miniflare/r2-data',
+});
+
+const bindings = await mf.getBindings();
+
+const r2Proxy = /** @type {R2Bucket} */ (bindings.R2_DEFAULT);
+
+console.log(r2Proxy);
+
+console.log("Uploading...");
+await r2Proxy.put("test.txt", "Hello World"); // This gets stuck indefinitely
+console.log("Uploaded test.txt to default bucket");
